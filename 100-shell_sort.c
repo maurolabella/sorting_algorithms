be2d6537fa3283348d96277767a1aedd6758bf2e
maurolabella <maurolabella@yahoo.com>
2022-06-13 12:10:02 -0300

    a

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -12,21 +12,24 @@ void shell_sort(int *array, size_t size)
 	int i, j, k, incr;
 	long int n;
 
-	n = size;
-	incr = 1;
-	while (incr <= (n - 1) / 9)
-		incr = 3 * incr + 1;
-
-	while (incr >= 1)
+	if (array && size > 1)
 	{
-		for (i = incr; i < n; i++)
+		n = size;
+		incr = 1;
+		while (incr <= (n - 1) / 9)
+			incr = 3 * incr + 1;
+
+		while (incr >= 1)
 		{
-			k = array[i];
-			for (j = i - incr; j >= 0 && k < array[j]; j = j - incr)
-				array[j + incr] = array[j];
-			array[j + incr] = k;
+			for (i = incr; i < n; i++)
+			{
+				k = array[i];
+				for (j = i - incr; j >= 0 && k < array[j]; j = j - incr)
+					array[j + incr] = array[j];
+				array[j + incr] = k;
+			}
+			incr = incr / 3;
+			print_array(array, size);
 		}
-		incr = incr / 3;
-		print_array(array, size);
 	}
 }
